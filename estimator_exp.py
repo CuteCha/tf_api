8c8994fc14d00261ff0b583a1f93550f107dc9c3
cxq <caoxiaoqiang@xiaomi.com>
2019-08-19 17:28:56 +0800

    estimator

--- estimator_exp.py
+++ estimator_exp.py
@@ -91,16 +91,12 @@ def lr_model_fn(features, labels, mode):
 
 
 def my_model(features, labels, mode, params):
-    """DNN with three hidden layers and learning_rate=0.1."""
-    # Create three fully connected layers.
     net = tf.feature_column.input_layer(features, params['feature_columns'])
     for units in params['hidden_units']:
         net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
 
-    # Compute logits (1 per class).
     logits = tf.layers.dense(net, params['n_classes'], activation=None)
 
-    # Compute predictions.
     predicted_classes = tf.argmax(logits, 1)
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
@@ -110,10 +106,8 @@ def my_model(features, labels, mode, params):
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    # Compute loss.
     loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
-    # Compute evaluation metrics.
     accuracy = tf.metrics.accuracy(labels=labels,
                                    predictions=predicted_classes,
                                    name='acc_op')
@@ -124,7 +118,6 @@ def my_model(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(
             mode, loss=loss, eval_metric_ops=metrics)
 
-    # Create training op.
     assert mode == tf.estimator.ModeKeys.TRAIN
 
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
